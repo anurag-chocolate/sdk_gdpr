3816322c06e8561889e8b451c0dd2776dbcc7357
Sourav <souravbanik.19.87@hotmail.com>
2018-05-16 14:49:59 +0530

    gdpr

--- app/src/main/java/com/vdopia/interstitialsample/MainActivity.java
+++ app/src/main/java/com/vdopia/interstitialsample/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.vdopia.ads.lw.Chocolate;
 import com.vdopia.ads.lw.LVDOAdRequest;
 import com.vdopia.ads.lw.LVDOConstants;
 import com.vdopia.ads.lw.LVDOInterstitialAd;
@@ -27,28 +29,50 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends RequestPermissionActivity implements RewardedAdListener {
     private LVDORewardedAd mRewardedAd;
+    //private LVDOInterstitialAd mInterstitialAd;
     private static final String TAG = "RewardedActivity";
     Button CLICK;
     Button Load;
     TextView STRING;
 
-    LVDOAdRequest adRequest = new LVDOAdRequest(this);
+     public int flag=0;
+     public int completedflag=0;
+      String locale;
+
+    LVDOAdRequest adRequestRewarded = new LVDOAdRequest(this);
+
+   // LVDOAdRequest adRequestInt = new LVDOAdRequest(this);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
+
         CLICK=(Button) findViewById(R.id.button);
         Load=(Button) findViewById(R.id.button2);
-        //CLICK=(Button) findViewById();
+
         STRING=(TextView) findViewById(R.id.textView4);
-        CLICK.setVisibility(View.INVISIBLE);
+        CLICK.setVisibility(View.VISIBLE);
         STRING.setVisibility(View.INVISIBLE);
-        prefetch();
-        Load.setVisibility(View.VISIBLE);
+
+        Load.setVisibility(View.INVISIBLE);
+        loadRewarded();
+        //prefetchInterstitial();
+
+       /* if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            locale = getResources().getConfiguration().getLocales().get(0).getCountry();
+        } else {
+            locale = getResources().getConfiguration().locale.getCountry();
+        }
+
+        Log.d(TAG, "country::-"+locale);
+
+        Chocolate.getGDPRConsentString(this);
+        Chocolate.isSubjectToGDPR(this);
+        Chocolate.setGDPR(this,true,"BOEFEAyOEFEAyAHBDENAIB9vABAAJA");*/
+        Chocolate.setGDPR(this,false,"BOEFEAyOEFEAyAHBDENAIB9vABAAJA");
     }
 
     @Override
@@ -58,27 +82,56 @@ public class MainActivity extends RequestPermissionActivity implements RewardedA
         return true;
     }
 
-    public void prefetch(){
+    public void prefetchRewarded(){
 
 
-        LVDORewardedAd.prefetch(this, "0DqXTL", adRequest);
+        LVDORewardedAd.prefetch(this, "0DqXTL", adRequestRewarded);
 
     }
 
-    public  void loadAd(View v){
+   /* private void prefetchInterstitial(){
+
+        LVDOInterstitialAd.prefetch(this,"0DqXTL", adRequestInt);
+
+
+    }*/
+
+public void loadAd(View v){
         loadRewarded();
 
-    }
+        /*if(completedflag==2)
+        {
+
+            loadRewarded();
+
+        }
+        else if (completedflag==3){
+
+            loadRewarded();
+
+        }*/
+
+
+}
+
+
     public void click(View v){
 
         Toast.makeText(getBaseContext(), "To mainactivity2", Toast.LENGTH_SHORT).show();
 
-        try {
-            mRewardedAd.showRewardAd("JK69GBeXDtYOldrZ", "Chocolate1", "indiancoin_"+mRewardedAd.getWinningPartnerName(), "22");
-        } catch(com.vdopia.ads.lw.ChocolateAdException e){
-            Log.e("SouravTest", "show failed ",e);
-        }
+        showRewarded();
+           /* if (flag==1)
+            {
 
+                showRewarded();
+
+            }
+            else if (flag!=1 && flag==2){
+
+
+                showInterstitial();
+
+            }*/
 
     }
     @Override
@@ -92,25 +145,72 @@ public class MainActivity extends RequestPermissionActivity implements RewardedA
 
 
 
-        adRequest.setDmaCode("807");
-        adRequest.setEthnicity("Asian");
-        adRequest.setPostalCode("110096");
-        adRequest.setCurrPostal("201301");
-        adRequest.setDmaCode("807");
-        adRequest.setAge("27");
+        adRequestRewarded.setDmaCode("807");
+        adRequestRewarded.setEthnicity("Asian");
+        adRequestRewarded.setPostalCode("110096");
+        adRequestRewarded.setCurrPostal("201301");
+        adRequestRewarded.setDmaCode("807");
+        adRequestRewarded.setAge("27");
 
-        adRequest.setGender(LVDOAdRequest.LVDOGender.MALE);
+        adRequestRewarded.setGender(LVDOAdRequest.LVDOGender.MALE);
 
 
-        adRequest.setRequester("Vdopia");
+        adRequestRewarded.setRequester("Vdopia");
 
-        adRequest.setAppDomain("vdopia.com");
+        adRequestRewarded.setAppDomain("vdopia.com");
 
-        adRequest.setAppStoreUrl("play.google.com");
-        adRequest.setCategory("Education");
-        adRequest.setPublisherDomain("vdopia.com");
-        mRewardedAd.loadAd(adRequest);
+        adRequestRewarded.setAppStoreUrl("play.google.com");
+        adRequestRewarded.setCategory("Education");
+        adRequestRewarded.setPublisherDomain("vdopia.com");
+        mRewardedAd.loadAd(adRequestRewarded);
         Toast.makeText(getBaseContext(), "LOAD AD event called", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+    /*private void loadInterstitial() {
+
+        //mInterstitialAd = new LVDOInterstitialAd(this, "8WeCrB", this);
+        mInterstitialAd = new LVDOInterstitialAd(this, "0DqXTL", this);
+
+        Log.d(TAG, "Method loadcalled");
+
+        adRequestInt.setDmaCode("807");
+        adRequestInt.setEthnicity("Asian");
+        adRequestInt.setPostalCode("110096");
+        adRequestInt.setCurrPostal("201301");
+        adRequestInt.setDmaCode("807");
+        adRequestInt.setAge("27");
+
+        adRequestInt.setGender(LVDOAdRequest.LVDOGender.MALE);
+
+
+        adRequestInt.setRequester("Vdopia");
+        //adRequest.setAppBundle("chocolateApp");
+        adRequestInt.setAppDomain("vdopia.com");
+        //adRequest.setAppName("VdopiaSampleApp");
+        adRequestInt.setAppStoreUrl("play.google.com");
+        adRequestInt.setCategory("Education");
+        adRequestInt.setPublisherDomain("vdopia.com");
+
+        mInterstitialAd.loadAd(adRequestInt);
+    }
+
+    private  void showInterstitial() {
+
+        try {
+            mInterstitialAd.show();
+        } catch(com.vdopia.ads.lw.ChocolateAdException e){
+            Log.e("", "show failed ",e);
+        }
+    }*/
+    private  void showRewarded() {
+        try {
+            mRewardedAd.showRewardAd("JK69GBeXDtYOldrZ", "Chocolate1", "indiancoin_"+mRewardedAd.getWinningPartnerName(), "22");
+        } catch(com.vdopia.ads.lw.ChocolateAdException e){
+            Log.e("SouravTest", "show failed ",e);
+        }
 
     }
 
@@ -119,9 +219,8 @@ public class MainActivity extends RequestPermissionActivity implements RewardedA
     public void onRewardedVideoLoaded(LVDORewardedAd rewardedAd){
 
         Log.d(TAG, "onRewardedVideoLoaded");
-        CLICK.setVisibility(View.VISIBLE);
-        Load.setVisibility(View.INVISIBLE);
 
+flag=1;
 
     }
 
@@ -130,11 +229,15 @@ public class MainActivity extends RequestPermissionActivity implements RewardedA
     public void onRewardedVideoFailed(LVDORewardedAd rewardedAd, LVDOConstants.LVDOErrorCode errorCode){
 
         Log.d(TAG, "onRewardedVideoFailed");
+        //loadInterstitial();
     }
     @Override
     public void onRewardedVideoShown(LVDORewardedAd rewardedAd){
-        Log.d(TAG, "onRewardedVideoShown");
-        prefetch();
+
+            flag=0;
+            prefetchRewarded();
+        Load.setVisibility(View.INVISIBLE);
+        Log.d(TAG, "Method onRewardedVideoShown");
 
     }
 
@@ -149,6 +252,8 @@ public class MainActivity extends RequestPermissionActivity implements RewardedA
     @Override
     public void onRewardedVideoDismissed(LVDORewardedAd rewardedAd){
         Log.d(TAG, "onRewardedVideoDismissed");
+        completedflag=2;
+        Load.setVisibility(View.VISIBLE);
     }
 
     // The rewarded video ad is being dismissed. Resume / load state accordingly.
@@ -156,9 +261,50 @@ public class MainActivity extends RequestPermissionActivity implements RewardedA
     public void onRewardedVideoCompleted(LVDORewardedAd rewardedAd){
 
         Log.d(TAG, "onRewardedVideoCompleted");
-        CLICK.setVisibility(View.VISIBLE);
-        Load.setVisibility(View.INVISIBLE);
+
+
     }
+
+  /*  @Override
+    public void onInterstitialLoaded(LVDOInterstitialAd interstitialAd) {
+
+        flag=2;
+        Log.d(TAG, "Method onInterstitialLoaded");
+
+    }
+
+    @Override
+    public void onInterstitialFailed(LVDOInterstitialAd interstitialAd, LVDOConstants.LVDOErrorCode errorCode) {
+
+        Log.d(TAG, "Method onInterstitialFailed");
+    }
+
+    @Override
+    public void onInterstitialShown(LVDOInterstitialAd interstitialAd) {
+        Load.setVisibility(View.INVISIBLE);
+        //prefetchInterstitial();
+        prefetchRewarded();
+
+        Log.d(TAG, "Method onInterstitialShown");
+
+    }
+
+    @Override
+    public void onInterstitialClicked(LVDOInterstitialAd interstitialAd) {
+
+
+        Log.d(TAG, "Method onInterstitialClicked");
+    }
+
+    @Override
+    public void onInterstitialDismissed(LVDOInterstitialAd interstitialAdl) {
+        completedflag=3;
+        Load.setVisibility(View.VISIBLE);
+
+        Log.d(TAG, "Method onInterstitialDismissed");
+
+    }
+*/
 
 
 
